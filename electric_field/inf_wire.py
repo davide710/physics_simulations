12c383e7cc5abe5d9bd9e1123758470873db0318
davide710 <davidemartinellii2003@gmail.com>
2024-04-18 18:32:32 +0200

    not working anymore

--- electric_field/inf_wire.py
+++ electric_field/inf_wire.py
@@ -6,7 +6,6 @@ from dolfinx.io import gmshio
 from mpi4py import MPI
 from dolfinx.fem.petsc import LinearProblem
 from dolfinx.fem import Function, FunctionSpace, Constant, Expression, locate_dofs_topological, dirichletbc
-from plotting import plot_scalar_function, plot_vector_function
 from dolfinx.mesh import compute_midpoints, locate_entities_boundary
 
 
@@ -35,12 +34,12 @@ div (v * grad V) - dot(grad V, grad v) = - rho / eps * v
 dot(grad V, grad v) - bc = rho / eps * v
 """
 
-eps = Constant(domain, 8.85e-12)
+eps = 8.85e-12
 
 tdim = domain.topology.dim
 facets = locate_entities_boundary(domain, tdim - 1, lambda x: np.full(x.shape[1], True))
 dofs = locate_dofs_topological(Function_space, tdim - 1, facets)
-bc = dirichletbc(default_scalar_type(0), dofs, Function_space)
+bc = dirichletbc(default_scalar_type(450000), dofs, Function_space)
 
 rho = 1e-3
 
@@ -55,5 +54,8 @@ problem = LinearProblem(a, L, bcs=[bc])
 V = Function(Function_space)
 V = problem.solve()
 
-print(f_di(V, np.array([0.15, 0, 0.5]), domain))
+with open('results_x.csv', 'w') as f:
+    f.write(f'r,V\n')
+    for r in np.arange(0.05, 0.95, 0.05):
+        f.write(f'{r},{f_di(V, np.array([0, r, 5]), domain)[0]}\n')
 
